9c262001c2b8eeb23c85924d25d271496bd77b89
BorisDmitriev <113904873+BorisDmitriev@users.noreply.github.com>
2023-10-10 19:07:34 +0200

    Initialize stage/03-routing-error-handling

---
+++ app.js
@@ -0,0 +1,79 @@
+/** EXTERNAL DEPENDENCIES */
+// Define "require"
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+
+/** ROUTERS */
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const recordsRouter = require('./routes/records');
+const ordersRouter = require('./routes/orders');
+const { setCors } = require("./middleware/security");
+
+/** INIT */
+const app = express();
+
+/** LOGGING */
+app.use(logger('dev'));
+
+
+/** SETTING UP LOWDB */
+
+import { join, dirname } from "path";
+import { Low, JSONFile } from "lowdb";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+// Use JSON file for storage
+const file = join(__dirname, "data/db.json");
+
+const adapter = new JSONFile(file);
+const db = new Low(adapter);
+
+db.defaults({
+    records: [],
+    users: [],
+    orders: []
+}).write();
+
+
+/** REQUEST PARSERS */
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(setCors);
+
+/** STATIC FILES*/
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+/** ROUTES */
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/records', recordsRouter);
+app.use('/orders', ordersRouter);
+
+/** ERROR HANDLING */
+app.use(function (req, res, next) {
+    const error = new Error('Looks like something broke...');
+    error.status = 400;
+    next(error);
+});
+
+app.use(function (err, req, res, next) {
+    res.send({
+        error: {
+            message: err.message
+        }
+    });
+});
+
+
+/** EXPORT PATH */
+module.exports = app;
